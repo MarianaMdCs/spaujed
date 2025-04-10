3f513d75efe0db47be67bc1f500b2fcaa33361b6
MarianaMdCs <mariana.mercado.isw@unipolidgo.edu.mx>
2025-04-10 10:46:32 -0600

    cambios

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -1,13 +1,30 @@
 import SidebarAdmins from "@/components/general/SidebarAdmins"
 import { SidebarProvider } from "@/components/ui/sidebar"
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import type React from "react"
 
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await auth();
+
+  let user = null;
+  if (session?.user?.id) {
+    user = await prisma.user.findUnique({
+      where: {
+        id: session.user.id,
+      },
+    });
+  }
+
+  if (user?.role == "ADMIN") {
+    redirect("/admin")
+  }
   return (
     <SidebarProvider className="flex min-h-screen">
       <SidebarAdmins />
--- src/app/(dashboard)/agremiado/layout.tsx
+++ src/app/(dashboard)/agremiado/layout.tsx
@@ -1,13 +1,30 @@
 import SidebarMembers from "@/components/general/SidebarMembers"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import type React from "react"
+import { prisma } from "@/lib/prisma"
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await auth();
+
+  let user = null;
+  if (session?.user?.id) {
+    user = await prisma.user.findUnique({
+      where: {
+        id: session.user.id,
+      },
+    });
+  }
+
+  if (user?.role == "MEMBER") {
+    redirect("/agremiado")
+  }
   return (
     <SidebarProvider className="flex min-h-screen">
       <SidebarMembers />
